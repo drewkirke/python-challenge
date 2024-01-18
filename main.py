e0ce560391cf9318bf0cbd8fce857948567f51a3
drewkirke <152921668+drewkirke@users.noreply.github.com>
2024-01-17 21:01:55 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
+import csv
 import os
-import pandas as pd
 
-file_path = "resources/election_data.csv"
-df = pd.read_csv(file_path)
+INPUT_PATH = os.path.join("Resources", "budget_data.csv")
+OUTPUT_PATH = os.path.join("analysis", "budget_analysis.txt")
 
-total_votes = len(df)
+month_counter = 0
+total_profit = 0
+change_list = []
+month_list = []
 
-candidate_data = df['Candidate'].value_counts().reset_index()
-candidate_data.columns = ['Candidate', 'Votes']
-candidate_data['Percentage'] = (candidate_data['Votes'] / total_votes) * 100
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+with open(INPUT_PATH) as csvfile:
+    csvreader = csv.reader(csvfile)
+    next(csvreader)
+    for row in csvreader:
+        month_counter = month_counter + 1
+        month_list.append(row[0])
+        current_profit = int(row[1])
+        total_profit = total_profit + current_profit
+        if month_counter > 1:
+            change = int(row[1])-previous_value
+            change_list.append(change)
+        previous_value = int(row[1])   
 
-winner = candidate_data.loc[candidate_data['Votes'].idxmax()]['Candidate']
+average_change = round(sum(change_list)/len(change_list),2)
 
-print("Election Results")
-print("-------------------------")
-print(f"Total Votes: {total_votes}")
-print("-------------------------")
-for index, row in candidate_data.iterrows():
-    print(f"{row['Candidate']}: {row['Percentage']:.3f}% ({row['Votes']})")
-print("-------------------------")
-print(f"Winner: {winner}")
-print("-------------------------")
+max_increase = max(change_list)
+max_decrease = min(change_list)
+
+max_index = change_list.index(max_increase)
+min_index = change_list.index(max_decrease)
+
+max_increase_month = month_list[max_index + 1]
+max_decrease_month = month_list[min_index + 1]
+
+output_text = (
+f"Financial Analysis\n"
+f"----------------------------\n"
+f"Total Months: {month_counter}\n"
+f"Total: ${total_profit}\n"
+f"Average Change: ${average_change}\n"
+f"Greatest Increase in Profits: {max_increase_month} (${max_increase})\n"
+f"Greatest Decrease in Profits: {max_decrease_month} (${max_decrease})"
+)
+
+print(output_text)
+with open(OUTPUT_PATH,"w") as outputfile:
+    outputfile.write(output_text)
