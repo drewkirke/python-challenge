2c51be2f3d7cddd77a01ec4bcab5a452e763a505
drewkirke <152921668+drewkirke@users.noreply.github.com>
2024-01-17 21:00:16 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+
+file_path = "resources/election_data.csv"
+df = pd.read_csv(file_path)
+
+total_votes = len(df)
+
+candidate_data = df['Candidate'].value_counts().reset_index()
+candidate_data.columns = ['Candidate', 'Votes']
+candidate_data['Percentage'] = (candidate_data['Votes'] / total_votes) * 100
+
+winner = candidate_data.loc[candidate_data['Votes'].idxmax()]['Candidate']
+
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+for index, row in candidate_data.iterrows():
+    print(f"{row['Candidate']}: {row['Percentage']:.3f}% ({row['Votes']})")
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
